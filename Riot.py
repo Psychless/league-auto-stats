d23806950ed95f7982f91b84d45ccc313fc1db85
kristaps.baumanis <kristaps.baumanis@autentica.lv>
2019-10-25 18:52:36 +0300

    Add player stat fetching & champion fetching

--- Riot.py
+++ Riot.py
@@ -13,6 +13,10 @@ def get_opponent_team_stats(match: dict, side: str):
     return get_team_stats(match, opponent_side)
 
 
+def get_participant_by_index(match: dict, side: str, index: int):
+    index += 0 if side == 'B' else 5  # Red team's player indexes are offset by 5
+    return match['participants'][index]
+
 def format_game_duration(game_duration: int):
     return time.strftime('%M.%S', time.gmtime(game_duration))
 
@@ -25,3 +29,15 @@ class RiotAPI:
 
     def fetch_match(self, match_id: int):
         return self.watcher.match.by_id(REGION, match_id)
+
+    def fetch_champion(self, match, champ_key: str):
+        dd_champ_version = self.watcher.data_dragon.versions_for_region('euw')['n']['champion']
+        champion_json = self.watcher.data_dragon.champions(dd_champ_version)
+        champion = None
+
+        for k, v in champion_json['data'].items():
+            if v['key'] == str(champ_key):
+                champion = v
+                break
+
+        return champion
--- Sheets/TeamSheet.py
+++ Sheets/TeamSheet.py
@@ -5,14 +5,15 @@ import Riot
 
 class TeamSheet(SheetBase):
     def fill_empty_matches(self, riot_API):
+        filled_match_count = 0
         row_index: int = GAME_STAT_STARTING_ROW
         match_id: int
         print('Looking for unfilled games..')
         while True:
             row_string = str(row_index)
-            row_values = self.get_row_values('A' + row_string + ':' + GAME_STAT_MATCH_ID_COL + row_string)
-            game_result = self.get_row_cell_value(row_values, GAME_STAT_RESULT_COL)
-            side = self.get_row_cell_value(row_values, GAME_STAT_SIDE_COL)
+            general_info_row = self.get_row_values('A' + row_string + ':' + COL_GAME_STAT_MATCH_ID + row_string)
+            game_result = self.get_row_cell_value(general_info_row, COL_GAME_STAT_RESULT)
+            side = self.get_row_cell_value(general_info_row, COL_GAME_STAT_SIDE)
 
             # Match has 'side' cell filled
             if side:
@@ -22,22 +23,40 @@ class TeamSheet(SheetBase):
                     continue
                 # Match is set and needs to be filled
                 if side and not game_result:
-                    match_id = self.get_row_cell_value(row_values, GAME_STAT_MATCH_ID_COL)
-                    self.fill_match_stats(riot_API, match_id, row_values, row_string, side)
+                    match_id = self.get_row_cell_value(general_info_row, COL_GAME_STAT_MATCH_ID)
+                    self.fill_all_stats(riot_API, match_id, general_info_row, row_string, side)
+                    filled_match_count += 1
                     row_index += 1
                     continue
             else:
                 print('All matches have been iterated')
-                print('Total matches found - ' + str(row_index - int(GAME_STAT_STARTING_ROW)))
+                print('Matches filled - ' + str(filled_match_count))
                 break
 
-    def fill_match_stats(self, riot_API, match_id: int, row_values: list, row_index, side: str):
-        print('Filling match - %s' % match_id)
+    def fill_all_stats(self, riot_API, match_id: int, general_info_row: list, row_index, side: str):
+        # Fetch stats from Riot
+        print('Fetching match #' + str(match_id) + ' stats')
         match = riot_API.fetch_match(match_id)
         team_stats = Riot.get_team_stats(match, side)
 
-        # Fill game's general info
-        self.set_row_cell_value(row_values, GAME_STAT_GAME_TIME_FORMATTED_COL, Riot.format_game_duration(match['gameDuration']))
-        self.set_row_cell_value(row_values, GAME_STAT_GAME_TIME_COL, match['gameDuration'])
-        self.set_row_cell_value(row_values, GAME_STAT_RESULT_COL, RESULT_WIN if team_stats['win'] == 'Win' else RESULT_LOSE)
-        self.set_row_values(row_values)
+        # Start filling spreadsheet
+        print('Filling spreadsheet')
+        self.fill_match_stats(match, team_stats, general_info_row)
+        self.fill_player_stats(riot_API, side, match, team_stats, row_index)
+
+        # Reset active spreadsheet to 'Game stats'
+        self.worksheet = self.sheet.worksheet(WORKSHEET_GAME_STATS)
+
+    # Fill game's general info
+    def fill_match_stats(self, match, team_stats, general_info_row):
+        self.set_row_cell_value(general_info_row, COL_GAME_STAT_GAME_TIME_FORMATTED, Riot.format_game_duration(match['gameDuration']))
+        self.set_row_cell_value(general_info_row, COL_GAME_STAT_GAME_TIME, match['gameDuration'])
+        self.set_row_cell_value(general_info_row, COL_GAME_STAT_RESULT, RESULT_WIN if team_stats['win'] == 'Win' else RESULT_LOSE)
+        self.set_row_values(general_info_row)
+
+    # Fill all player stats
+    def fill_player_stats(self, riot_API, side, match, team_stats, row_index):
+        for i in range(0, 5):  # 0: top, 1: jungle.. etc.
+            self.worksheet = self.sheet.worksheet(ROLE_WORKSHEETS[i])
+            player = Riot.get_participant_by_index(match, side, i)
+            champion = riot_API.fetch_champion(match, player['championId'])
